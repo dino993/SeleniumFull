90030a800d1e93e910f3d4d2f2edaffb86bae4d1
Алексей Алтунин <avega993@mail.ru>
2016-11-16 00:52:38 +0300

    Добавил .Net проект

---
+++ SeleniumCSharp/FirstWebDriverProject/BaseTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Support.UI;
+
+namespace FirstWebDriverProject
+{
+	[TestFixture]
+	public class BaseTest
+	{
+		public IWebDriver driver;
+		public WebDriverWait wait;
+
+		[SetUp]
+		public void SetupDriver()
+		{
+			driver = new ChromeDriver();
+			wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+		}
+
+		[TearDown]
+		public void CloseDriver()
+		{
+			driver.Quit();
+			driver = null;
+		}
+
+	}
+}
+
---
+++ SeleniumCSharp/FirstWebDriverProject/Test.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace FirstWebDriverProject
+{
+	[TestFixture()]
+	public class Test : BaseTest
+	{
+		[Test()]
+		public void TestCase()
+		{
+			driver.Url = "http://www.google.com/";
+			driver.FindElement(By.Name("q")).SendKeys("webdriver");
+			driver.FindElement(By.Name("btnG")).Click();
+			wait.Until(ExpectedConditions.TitleIs("webdriver - Поиск в Google"));
+		}
+	}
+}
+
