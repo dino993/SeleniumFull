b6c1ec5081c29f415fa804890c9b669622385c4c
Алексей Алтунин <avega993@mail.ru>
2016-12-01 00:08:00 +0300

    Залил Задание 10. Проверить, что открывается правильная страница товара

--- SeleniumNodeJS/litecart/task10/checkProductParameters.js
+++ SeleniumNodeJS/litecart/task10/checkProductParameters.js
@@ -26,9 +26,149 @@ test.describe('Task 10', function() {
         });
     });
 
+    var productParameters = {
+        title: undefined,
+        prices: {
+            base: {
+                value: undefined,
+                style: {
+                    color: undefined,
+                    view: undefined,
+                    size: undefined
+                }
+            },
+            discount: {
+                value: undefined,
+                style: {
+                    color: undefined,
+                    view: undefined,
+                    size: undefined
+                }
+            }
+        }
+    }
+
     test.it('Task 10 - check product parameters', function() {
 
-        //in progress
+        driver.get("http://localhost/litecart/en/");
+
+        var categorySelector = "//*[@id='box-campaigns']";
+        var pruductSelector = "//li[@class='product column shadow hover-light']";
+        var titleSelector = "//div[@class='name']";
+        var priceSelector = "//div[@class='price-wrapper']";
+        var regularPriceSelector = "//s[@class='regular-price']";
+        var strongPriceSelector = "//strong[@class='campaign-price']";
+
+
+
+        driver.findElement(By.xpath(categorySelector + pruductSelector + titleSelector)).getText().then(function(title) {
+
+            productParameters.title = title;
+
+            driver.findElement(By.xpath(categorySelector + pruductSelector + priceSelector + regularPriceSelector)).then(function(regularPrice) {
+
+                regularPrice.getText().then(function(text) {
+                    productParameters.prices.base.value = text;
+
+                });
+
+                regularPrice.getCssValue("color").then(function(color) {
+                    productParameters.prices.base.style.color = color;
+                });
+
+                regularPrice.getSize().then(function(size) {
+                    productParameters.prices.base.style.size = size;
+                });
+
+                regularPrice.getCssValue("text-decoration").then(function(text_decoration) {
+                    productParameters.prices.base.style.view = text_decoration;
+                });
+
+            });
+
+            driver.findElement(By.xpath(categorySelector + pruductSelector + priceSelector + strongPriceSelector)).then(function(strongPrice) {
+
+                strongPrice.getText().then(function(text) {
+                    productParameters.prices.discount.value = text;
+                });
+
+                strongPrice.getCssValue("color").then(function(color) {
+                    productParameters.prices.discount.style.color = color;
+                });
+
+                strongPrice.getSize().then(function(size) {
+                    productParameters.prices.discount.style.size = size;
+                });
+
+                strongPrice.getCssValue("font-weight").then(function(font_weight) {
+                    productParameters.prices.discount.style.view = font_weight;
+                });
+            });
+
+            // Выбираем товар.
+            driver.findElement(By.xpath(categorySelector + pruductSelector)).click();
+
+            driver.findElement(By.xpath("//h1[@class='title']")).getText().then(function(title) {
+
+                assert.equal(productParameters.title, title, "Ошибка! Заголовки отличаются");
+
+                driver.findElement(By.xpath(regularPriceSelector)).then(function(regularPrice) {
+
+                    regularPrice.getText().then(function(text) {
+                        assert.equal(productParameters.prices.base.value, text, "Ошибка! Обычные цены отличаются");
+                    });
+
+                    regularPrice.getCssValue("color").then(function(color) {
+                        try{
+                            assert.equal(productParameters.prices.base.style.color, color);
+                        } catch(ex) {
+                            console.log( "\nОшибка! Обычные цены Цвета отличаются")
+                        }
+                    });
+
+                    regularPrice.getSize().then(function(size) {
+                        try{
+                            assert.equal(productParameters.prices.base.style.size, size);
+                        } catch(ex) {
+                            console.log("\nОшибка! Обычные цены Размеры отличаются")
+                        }
+
+                    });
+
+                    regularPrice.getCssValue("text-decoration").then(function(text_decoration) {
+                        assert.equal(productParameters.prices.base.style.view, text_decoration, "Ошибка! Обычные цены Перечеркнутость отличаются");
+                    });
+
+                });
+
+                driver.findElement(By.xpath(strongPriceSelector)).then(function(strongPrice) {
+
+                    strongPrice.getText().then(function(text) {
+                        assert.equal(productParameters.prices.discount.value, text, "Ошибка! Скидочные цены отличаются");
+                    });
+
+                    strongPrice.getCssValue("color").then(function(color) {
+                        assert.equal(productParameters.prices.discount.style.color, color, "Ошибка! Скидочные цены Цвета отличаются");
+                    });
+
+                    strongPrice.getSize().then(function(size) {
+                        try{
+                            assert.equal(productParameters.prices.discount.style.size, size);
+                        } catch(ex) {
+                            console.log("\nОшибка! Скидочные цены Размеры отличаются")
+                        }
+
+                    });
+
+                    strongPrice.getCssValue("font-weight").then(function(font_weight) {
+                        assert.equal(productParameters.prices.discount.style.view, font_weight, "Ошибка! Скидочные цены Жирность шрифта отличаются");
+                    });
+                });
+
+            });
+
+        });
+
     });
 
     test.after(function() {
